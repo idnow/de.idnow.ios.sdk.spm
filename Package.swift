0589fc929f0303262469d10fe71d3f4e524a5e30
Seyit Kaya <seyit.kaya@idnow.io>
2024-09-05 13:28:33 +0200

    SPM dependency integration

--- Package.swift
+++ Package.swift
@@ -1,22 +1,27 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "IDNowSDKCore",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "IDNowSDKCore-with-NFC",
             targets: [
-                "IDNowSDKCore-with-NFC", "FaceTecSDK", "XS2AiOSNetService"
+                "IDNowSDKCore-with-NFC", "FaceTecSDK", "XS2AiOSNetService", "IDNowSDKWrapper"
             ]),
         .library(
             name: "IDNowSDKCore-without-NFC",
             targets: [
-                "IDNowSDKCore-without-NFC", "FaceTecSDK", "XS2AiOSNetService"
+                "IDNowSDKCore-without-NFC", "FaceTecSDK", "XS2AiOSNetService", "IDNowSDKWrapper"
             ])
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/unissey/sdk-ios.git",
+            from: "3.1.4"
+        )
+
     ],
     targets: [
         .binaryTarget(
@@ -35,6 +40,13 @@ let package = Package(
             name: "XS2AiOSNetService",
             url: "https://github.com/FinTecSystems/xs2a-ios-netservice/releases/download/1.0.7/XS2AiOSNetService.xcframework.zip",
             checksum: "987e1e075fc9ca8b2f22cb60b436b84cc1304bb7c03cbce967979b588e5e8868"
+        ),
+        .target(
+            name: "IDNowSDKWrapper",
+            dependencies: [
+                .product(name: "UnisseySdk", package: "sdk-ios")
+            ],
+            path: "Sources/IDNowSDKWrapper"
         )
     ],
     swiftLanguageVersions: [.v5]
