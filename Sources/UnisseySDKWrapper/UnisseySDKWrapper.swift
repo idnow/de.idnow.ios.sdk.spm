3b2a1b6858b1779dbfe32dea9f6dbad60a35c514
Seyit Kaya <seyit.kaya@idnow.io>
2025-10-16 08:21:12 +0200

    - clean up

--- Sources/UnisseySDKWrapper/UnisseySDKWrapper.swift
+++ Sources/UnisseySDKWrapper/UnisseySDKWrapper.swift
@@ -1,10 +1,15 @@
+//
+//  UnisseySDKWrapper.swift
+//
+//  Created by Seyit Kaya on 16.10.25.
+//
 
 import UIKit
 import SwiftUI
 import UnisseySdk
 
 /// Swift Package Manager needs at least one source file
-public class IDnowSDKWrapper {
+public class UnisseySDKWrapper {
     public static func handlePackage() {
         let sdkWrapper = UnisseyView(
             unisseyViewModel: UnisseyViewModel(
